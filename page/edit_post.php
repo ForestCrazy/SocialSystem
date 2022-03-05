b91ce5b462f49c05a8866831ed8fe7a6ceb246f5
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-03-05 13:00:17 +0700

    Update edit_post.php

--- page/edit_post.php
+++ page/edit_post.php
@@ -20,7 +20,9 @@ if (!isset($_SESSION['user_id'])) {
 if (isset($_POST['submit_edit'])) {
     $filename = $_POST['post_img'];
     if ($_FILES['uploadFile']['name'] != NULL) {
-        unlink($filename);
+        if ($filename != "") {
+            unlink($filename);
+        }
         $filename = "images/post/" . time() . $_FILES['uploadFile']['name'];
         move_uploaded_file($_FILES['uploadFile']['tmp_name'], $filename);
     }
